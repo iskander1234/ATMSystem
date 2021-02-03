d207b052eb94a8c54f8cc74668f91f20665c9c8d
Iskander Dosgali <idosgali@gmail.com>
2021-02-03 23:38:41 +0600

    Изменил класс Main

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,21 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-    }
+//        Bank bank = new Bank();
+//        bank.createPin(bank);
 
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+        Bank tiger = context.getBean("tiger", Bank.class);
+        tiger.outPut();
+        System.out.println("tiger = " + tiger);
+
+    }
 }
