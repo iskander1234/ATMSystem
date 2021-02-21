094f7c885d2d08dc5802dee4797df500c903168b
Iskander Dosgali <idosgali@gmail.com>
2021-02-21 23:41:04 +0600

    Поставил Component,Autowired,PostConstruct,PreDestroy  на класс ATM

--- src/main/java/com/example/demo/ATM.java
+++ src/main/java/com/example/demo/ATM.java
@@ -1,8 +1,13 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.sql.*;
 import java.util.Scanner;
-
+@Component("Service")
 public  class ATM implements  BankService{
 
     public Client client;
@@ -10,15 +15,20 @@ public  class ATM implements  BankService{
     Connection connection = null;
     Statement statement = null;
     Scanner scan = new Scanner(System.in);
-
+    @Autowired
     public ATM(Bank bank) {
         this.bank = bank;
     }
 
+    public ATM() {
+
+    }
+
     @Override
     public void createPin(Client client){
-        String card_num = scan.next();
-        if (card_num == client.getCard_num()) {
+        this.client = client;
+        System.out.println("Генерированный счет клиента: "+ client.getCard_num());
+        if (true){
             int length = client.getCard_num().length();
             if (length == 6){
                 if (client != null){
@@ -27,14 +37,14 @@ public  class ATM implements  BankService{
                         int num = scan.nextInt();
                         switch (num) {
                             case (1):
-                                 System.out.println("How much do you want to top up?");
-                                 int setmoney = scan.nextInt();
-                                 setMoney(setmoney);
-                                break;
-                            case (2):
-                                System.out.println("How much do you want to top up?");
+                                System.out.println("На сколько вы хотите пополнить?");
                                 int getmoney = scan.nextInt();
                                 getMoney(getmoney);
+                                break;
+                            case (2):
+                                System.out.println("На сколько вы хотите снять?");
+                                int setmoney = scan.nextInt();
+                                setMoney(setmoney);
                                 break;
                             case (3):
                                 printInfo();
@@ -61,7 +71,7 @@ public  class ATM implements  BankService{
             System.out.println("Средства на вашем счете: " + this.client.getCash());
         }
         else {
-            this.client.setCash(this.client.getCash() - cash);//Комиссия
+            this.client.setCash(this.client.getCash() - cash);
             try {
                 int total = client.getCash() - cash;
                 String query = "update clients set cash = '" + total + "' where client_id = " + client.getClient_id();
@@ -71,8 +81,8 @@ public  class ATM implements  BankService{
             catch (Exception e){
                 System.out.println(e);
             }
-            System.out.println("Извини, у тебя недостаточно денег.\n"+
-                    "Свободные средства на вашем счете: "+ client.getCash());
+            System.out.println("Операция прошла успешно\n"+
+                    "Доступные средства на вашем счете: "+ client.getCash());
         }
     }
 
@@ -96,7 +106,7 @@ public  class ATM implements  BankService{
 
     public void printInfo()
     {
-        System.out.println("Текущее состояние счёта: ");
+        System.out.println("Текущее состояние счёта");
         System.out.println("Номер счета: "+ client.getCard_num());
         System.out.println("Колчество средств на счёте: "+ client.getCash());
     }
@@ -121,8 +131,8 @@ public  class ATM implements  BankService{
         }
         return connection;
     }
-
-    public void init_method() throws SQLException {
+    @PostConstruct
+    public void init() throws SQLException {
         Connection connection = this.createDBConnection();
         ResultSet set = null;
         String query = "SELECT * FROM clients";
@@ -132,12 +142,11 @@ public  class ATM implements  BankService{
             Client client = new Client(
                     set.getInt(1),
                     set.getString(2),
-                    set.getInt(3),
-                    set.getString(4));
+                    set.getInt(3));
             bank.getClients().add(client);
         }
     }
-
+    @PreDestroy
     public void destroy() {
         try {
             connection.close();
