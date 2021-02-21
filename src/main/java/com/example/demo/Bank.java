bc242fc579d35f37f7438bbfcb731b6e004170d1
Iskander Dosgali <idosgali@gmail.com>
2021-02-21 23:39:52 +0600

    Поставил Component на класс Банка

--- src/main/java/com/example/demo/Bank.java
+++ src/main/java/com/example/demo/Bank.java
@@ -1,10 +1,14 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import java.util.List;
-
+@Component
 public class Bank{
-
+    @Autowired
     private List<Client> clients;
+
     public Bank() {
     }
 
