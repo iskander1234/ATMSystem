14aaf14109a2126a9ec08df014619f57c8238238
Iskander Dosgali <idosgali@gmail.com>
2021-02-21 23:42:16 +0600

    Изменил DemoApplication  добавил AnnotationConfigApplicationContext

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Random;
@@ -16,13 +17,23 @@ public class DemoApplication {
 
         //ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        BankService bankService = context.getBean("bankService", ATM.class);
+//        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+//        BankService bankService = context.getBean("Service", ATM.class);
+//        Random rand = new Random();
+//
+//        Client randomClient = bankService.getBank().getClients().get(rand.nextInt(bankService.getBank().getClients().size()));
+//        bankService.createPin(randomClient);
+//        ((ClassPathXmlApplicationContext) context).close();
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.scan("com.example.demo");
+        context.refresh();
+        BankService bankService = context.getBean("Service", ATM.class);
         Random rand = new Random();
 
         Client randomClient = bankService.getBank().getClients().get(rand.nextInt(bankService.getBank().getClients().size()));
         bankService.createPin(randomClient);
-        ((ClassPathXmlApplicationContext) context).close();
+        ((AnnotationConfigApplicationContext) context).close();
 
     }
 }
